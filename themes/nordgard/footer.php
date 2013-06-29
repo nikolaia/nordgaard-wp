de7f0ec25dd32b8a3c5d6861c8039c7669b00e49
Nikolai <nikolai@stasj.com>
2013-06-29 11:34:27 +0200

    Added footer images

--- themes/nordgard/footer.php
+++ themes/nordgard/footer.php
@@ -1,6 +1,15 @@
 
 	</div>
 
+	<div id="footerLinks">
+		<a href="http://www.tromsfylke.no/Forside/Logoer/tabid/2057/Default.aspx">
+			<img src="<?php bloginfo('template_url'); ?>/img/dyroey.png" alt="Logo Troms fylkeskommune" />
+		</a>
+		<a href="http://snl.no/Dyr%C3%B8y/kommune_i_Troms">
+			<img src="<?php bloginfo('template_url'); ?>/img/troms.png" alt="Logo Dyrøy kommune" />
+		</a>
+	</div>
+	
 	<?php wp_footer(); ?>
 
 	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
